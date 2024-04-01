1b0d8a7c3588faf0df3b7ee0aeff92430f2f2861
Ruella-De-Vil <159031808+Ruella-De-Vil@users.noreply.github.com>
2024-04-01 12:11:38 +0200

    Added js which uses search engine and api to display the location/city and current temperature when a city is searched

--- src/my-weather-app.js
+++ src/my-weather-app.js
@@ -1,10 +1,27 @@
+function currentWeatherConditions(response) {
+    let tempElement = document.querySelector("#temp");
+    let currentTemp = response.data.main.temp;
+    let locationElement = document.querySelector("#location");
+    
+    
+    locationElement.innerHTML = response.data.name;
+    tempElement.innerHTML = Math.round(currentTemp)
+}
+
+
+function searchLocation(location) {
+let apiKey = "a9498979f933b4259d63b76dd499f095";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(currentWeatherConditions);
+}
+
 function locationSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#location-search");
-    let locationElement = document.querySelector("#location");
-    locationElement.innerHTML = searchInput.value;
+    searchLocation(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", locationSearch);
 
+searchLocation("Durban")
